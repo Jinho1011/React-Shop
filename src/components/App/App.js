dabca344113aa6861583449028282eb622f46946
jinho1011 <gino9940@gmail.com>
2020-04-08 22:13:14 +0900

    Nav Updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,66 @@
-import React from "react";
-import "components/App/App.css";
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Nav from "components/Nav/Nav";
+import Cart from "components/Cart/Cart";
+import Main from "components/Main/Main";
+import Item from "components/Item/Item";
+import data from "./MOCK_DATA.json";
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            products: data,
+        };
+        this.renderFoodDetail = this.renderFoodDetail.bind(this);
+    }
+
+    renderFoodDetail() {
+        return this.state.products.map((product) => {
+            return (
+                <Route
+                    exact
+                    path={`/item/${product.id}`}
+                    render={(props) => {
+                        return (
+                            <div>
+                                asd
+                                {/* <Item
+                                    image={product.image}
+                                    name={product.name}
+                                    price={product.price}
+                                    id={product.id}
+                                    key={product.id}
+                                /> */}
+                            </div>
+                        );
+                    }}
+                />
+            );
+        });
+    }
+
+    componentDidMount() {
+        console.log(this.state.products);
+    }
+
+    render() {
+        return (
+            <div>
+                <Nav />
+                <Switch>
+                    <Route
+                        exact
+                        path="/"
+                        render={(props) => {
+                            return <Main products={this.state.products} />;
+                        }}
+                    />
+                    {this.renderFoodDetail()}
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "index.css";
-import { BrowserRouter } from "react-router-dom";
 import App from "components/App/App";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
     <BrowserRouter>
-        <App></App>
+        <App />
     </BrowserRouter>,
     document.getElementById("root")
 );
